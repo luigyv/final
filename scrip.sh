8ba7ff397f94cc12fb89954687c520db389bbbb8
luigyv <liugui306@gmail.com>
2024-05-17 17:16:37 -0500

    Update scrip.sh

--- scrip.sh
+++ scrip.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
-
 echo "FROM python:3" > Dockerfile
-
 echo "COPY requeriments.txt /home/." >> Dockerfile
-
 echo "RUN pip install -r /home/requeriments.txt" >> Dockerfile
-
 echo "EXPOSE 8020 " >> Dockerfile
-
 echo "WORKDIR /home/app/" >> Dockerfile
-
 echo "COPY server.py . " >> Dockerfile
-
 echo "CMD python3 server.py" >> Dockerfile
-
 docker stop samrun
-
 docker rm samrun
-
 docker build -t sample .
-
 docker run -t -d -p 8020:8020 --name samrun sample
-
 docker ps -a
-
